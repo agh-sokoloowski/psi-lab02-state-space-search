b23a060c0a00710f6cbf88e2dba6a118ce87ffd1
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-22 15:33:28 +0100

    A*

--- solvers/idastar.py
+++ solvers/idastar.py
@@ -13,19 +13,40 @@ class IDAStar(HeuristicSolver):
 
     
     def solve(self) -> Optional[Node]:
-        # TODO:
         # - if the root node is a goal, just return it
         #   tip. use 'is_goal' method from Problem
+        if self.problem.is_goal(self.root.state):
+            return self.root
         # - set bound to the heuristic of the start state
+        bound = self.heuristic(self.start)
         # - run self._cost_limited search
+        while True:
+            node, cost = self._cost_limited_search(self.root, bound)
         #   * if it returned a node, return it!
+            if node:
+                return node
         #   * if returned cost equals bound, return None
+            if cost == bound:
+                return None
         #   * otherwise update bound to cost and repeat the limited search
-        raise NotImplementedError()
+            bound = cost
 
     
     def _cost_limited_search(self, root: Node, bound: float) -> Tuple[Optional[Node], float]:
-        # TODO:
+        frontier = PriorityQueue(lambda node: node.cost + self.heuristic(node.state))
+        visited = {self.start}
+
+        frontier.push(root)
+        while frontier:
+            node = frontier.pop()
+            if self.problem.is_goal(node.state):
+                return node, node.cost
+            if node.cost > bound:
+                return None, node.cost
+            for child in self.tree.expand(self.problem, node):
+                if child not in self.visited:
+                    visited.add(child.state)
+                    frontier.push(child)
         # - do a DFS with depth limited by a given bound
         #   tip. base in on the BestFirstSearch you have already done by now
         # differences:
@@ -34,7 +55,6 @@ class IDAStar(HeuristicSolver):
         # - this method return a tuple:
         #   * first element is the node if the method finds a goal state, otherwise it should be None
         #   * second element is a new bound (the smallest fcost of a not expanded child)
-        raise NotImplementedError()
                 
     def search_tree(self) -> Tree:
         return self.tree
