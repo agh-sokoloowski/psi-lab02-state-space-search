39bdf5fb93e992d602397b31567885a488c592f5
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-22 17:35:19 +0100

    First try with positions

--- problems/n_puzzle/heuristics/n_puzzle_tiles_out_of_place_heuristic.py
+++ problems/n_puzzle/heuristics/n_puzzle_tiles_out_of_place_heuristic.py
@@ -6,10 +6,15 @@ from problems.n_puzzle.heuristics.n_puzzle_abstract_heuristic import NPuzzleAbst
 class NPuzzleTilesOutOfPlaceHeuristic(NPuzzleAbstractHeuristic):
 
     def __call__(self, state: NPuzzleState) -> float:
-        # TODO:
         # Calculate how many tiles are not on their expected positions
         # tip 1.'state' is the current state, 
         # tip 2. you can use self.positions function to get from it a dictionary:
         #   { tile_number : (x_coordinate, y_coordinate) }
         # tip 3. self.goal_coords contains such a dictionary for the goal state
-        raise NotImplementedError()
+        positions = self.positions(state)
+        wrong_positions = 0
+        for tile, position in self.goal_coords:
+            if position is not positions.get(tile):
+                wrong_positions += 1
+        
+        return wrong_positions
