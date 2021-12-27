275a58a1c1e4c9afd1ecded3d1d9d11e6f94e7c3
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-27 17:23:58 +0100

    Manhattan

--- problems/n_puzzle/heuristics/n_puzzle_manhattan_heuristic.py
+++ problems/n_puzzle/heuristics/n_puzzle_manhattan_heuristic.py
@@ -6,11 +6,14 @@ from problems.n_puzzle.heuristics.n_puzzle_abstract_heuristic import NPuzzleAbst
 class NPuzzleManhattanHeuristic(NPuzzleAbstractHeuristic):
 
     def __call__(self, state: NPuzzleState) -> float:
-        # TODO:
         # Calculate a manhattan distance between tiles and their expected places
         # The result should be sum of those distances. 
         # tip 1.'state' is the current state, 
         # tip 2. you can use self.positions function to get from it a dictionary:
         #   { tile_number : (x_coordinate, y_coordinate) }
         # tip 3. self.goal_coords contains such a dictionary for the goal state
-        raise NotImplementedError()
+        positions = self.positions(state)
+        sum = 0
+        for i in range(1, len(state.tiles) + 1):
+            sum += abs(positions[i][0] - self.goal_coords[i][0]) + abs(positions[i][1] - self.goal_coords[i][1])
+        return sum
