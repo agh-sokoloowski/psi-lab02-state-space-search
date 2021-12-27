ba8c01cfa969b43ef05f1cb2fb0d3e6a7f7b525d
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2021-12-27 18:02:32 +0100

    Diagonal move - another try

--- problems/grid_pathfinding/heuristics/diagonal_heuristic.py
+++ problems/grid_pathfinding/heuristics/diagonal_heuristic.py
@@ -15,6 +15,6 @@ class GridDiagonalHeuristic(Heuristic[GridCoord]):
         # - 'state' is the current state 
         # - 'self.problem.goal' is the goal state
         # - 'self.problem.diagonal_weight' is cost of making a diagonal move
-        delta_x = self.problem.diagonal_weight * abs(state.x - self.problem.goal.x)
-        delta_y = self.problem.diagonal_weight * abs(state.y - self.problem.goal.y)
-        return ((math.sqrt(2) - 1) * min(delta_x, delta_y)) + max(delta_x, delta_y)
+        delta_x = abs(state.x - self.problem.goal.x)
+        delta_y = abs(state.y - self.problem.goal.y)
+        return ((self.problem.diagonal_weight - 1) * min(delta_x, delta_y)) + max(delta_x, delta_y)
